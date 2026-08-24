4566c3c98d240be830dbe98bda9bfc4f4fb64a0d
Kash <kash@Kashs-MacBook-Pro.local>
2026-08-24 18:57:33 +0100

    mobile nav alginemnt fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ const LINKEDIN_URL = "https://www.linkedin.com/in/karishma-bhugoowan/";
 
 export default function Footer() {
   return (
-    <div className="relative mt-[140px] h-32 w-full border-t border-white/5 bg-bg-inverse">
-      <div className="absolute top-[41px] left-[100px] inline-flex flex-col items-start justify-start">
+    <div className="relative mt-[140px] flex h-32 w-full flex-col justify-center border-t border-white/5 bg-bg-inverse px-6 md:block md:px-0">
+      <div className="flex flex-col items-start justify-start md:absolute md:top-[41px] md:left-[100px]">
         <div className="flex flex-col items-start justify-start self-stretch">
           <div className="justify-start font-['Manrope'] text-lg leading-7 font-medium text-white/90">
             Karishma Bhugoowan
@@ -22,7 +22,7 @@ export default function Footer() {
         aria-label="LinkedIn"
         data-property-1="link"
         data-property-2="default"
-        className="absolute top-[51px] right-[100px] flex size-8 items-center justify-center rounded-md bg-white"
+        className="absolute top-1/2 right-6 flex size-8 -translate-y-1/2 items-center justify-center rounded-md bg-white md:top-[51px] md:right-[100px] md:translate-y-0"
       >
         <svg viewBox="0 0 24 24" className="size-4.5" fill="#1c1726">
           <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 1 1 0-4.125 2.062 2.062 0 0 1 0 4.125zM7.114 20.452H3.558V9h3.556v11.452z" />
